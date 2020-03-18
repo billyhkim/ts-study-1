ff5e5b807c6a53059f5f2a67e47189fbaad9e1bb
Billy Kim <billyhkim.dev@gmail.com>
2020-03-18 16:02:47 -0700

    added interface for Todo

--- fetch-json/index.ts
+++ fetch-json/index.ts
@@ -2,5 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// interfaces describe the structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url)
-  .then(res => console.log(res.data));
+  .then(res => {
+    const { id, title, completed } = res.data as Todo;
+
+    console.log(`
+      The Todo with ID: ${id}
+      Has a title of: ${title}
+      Is it finished? ${completed}
+    `);
+  });
