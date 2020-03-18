34b2d1370580152a933ce8c24bb0d0cda67e113c
Billy Kim <billyhkim.dev@gmail.com>
2020-03-18 16:06:57 -0700

    added check to logTodo argument types

--- fetch-json/index.ts
+++ fetch-json/index.ts
@@ -13,9 +13,13 @@ axios.get(url)
   .then(res => {
     const { id, title, completed } = res.data as Todo;
 
+    logTodo(id, title, completed);
+  });
+
+  const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
       The Todo with ID: ${id}
       Has a title of: ${title}
       Is it finished? ${completed}
     `);
-  });
+  };
