94bd4ce1ed48ddaad189a203e7e957c56a06abe7
Billy Kim <billyhkim.dev@gmail.com>
2020-03-22 15:58:44 -0700

    7 - interface example

---
+++ 7-interfaces/interfaces.ts
@@ -0,0 +1,14 @@
+// interface = new type describing property names and value types of obj
+const oldCivic = {
+  name: 'Civic',
+  year: 2000,
+  broken: true,
+};
+
+const printVehicle = (vehicle: { name: string; year: number; broken: boolean; }): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken: ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
