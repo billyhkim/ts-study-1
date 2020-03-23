beeeff69c295ea2c408f7de88a668eef5dd3616d
Billy Kim <billyhkim.dev@gmail.com>
2020-03-23 04:28:51 -0700

    9 - added google maps

--- 9-design-patterns/src/index.ts
+++ 9-design-patterns/src/index.ts
@@ -2,7 +2,12 @@ import { User } from './User';
 import { Company } from './Company';
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
