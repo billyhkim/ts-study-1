d1f35bc04a05f2363e948740e1a4cded4e02a2ac
Billy Kim <billyhkim.dev@gmail.com>
2020-03-22 21:01:28 -0700

    8 - classes example complete

--- 8-classes/classes.ts
+++ 8-classes/classes.ts
@@ -1,24 +1,34 @@
 // classes = blueprint to create an obj with fields (values) and methods (fxns) to represent a 'thing'
 class Vehicle {
-  drive(): void {
+  constructor(public color: string) {}
+  
+  public drive(): void {
     console.log('driving');
   }
 
-  honk(): void {
+  protected honk(): void {
     console.log('beep beep!');
   }
 }
 
+const sedan = new Vehicle('silver');
+sedan.drive();
+console.log(sedan.color);
+
 class Car extends Vehicle {
-  drive(): void {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  
+  private move(): void { // note: private methods aren't for security measures, just letting others know to be wary of using it
     console.log('car is driving');
+  }
+
+  ignitionAndDrive(): void {
+    this.move();
+    this.honk();
   }
 }
 
-const sedan = new Vehicle();
-sedan.drive();
-sedan.honk();
-
-const car = new Car();
-car.drive();
-car.honk();
+const car = new Car(4, 'white');
+car.ignitionAndDrive();
