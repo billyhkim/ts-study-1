ccd21b2e5dbed93bafe035d0292cecfe66e480cc
Billy Kim <billyhkim.dev@gmail.com>
2020-03-25 11:45:57 -0700

    10 - array sorting logic added

--- 10-sort/src/index.ts
+++ 10-sort/src/index.ts
@@ -1,7 +1,25 @@
-console.log('Hello world!');
+class Sorter {
+  // collection: number[];
+  // constructor(collection: number[]) {
+  //   this.collection = collection;
+  // }
+  constructor(public collection: number[]) {}
 
-const logger = () => {
-  console.log('123');
-};
+  sort(): void {
+    const { length } = this.collection;
 
-logger();
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
