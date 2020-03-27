c73d37d727ecb090c02e5b14688552a887dab8a6
Billy Kim <billyhkim.dev@gmail.com>
2020-03-27 12:40:53 -0700

    17 - adding state as constructor vs property

--- 17-react-redux/src/index.tsx
+++ 17-react-redux/src/index.tsx
@@ -5,9 +5,33 @@ interface AppProps {
   color?: string; // question mark indicates optional prop
 }
 
+interface AppState { // only required if initializing state with constructor
+  counter: number;
+}
+
 class App extends React.Component<AppProps> {
+  // constructor(props: AppProps) {
+  //   super(props);
+  //   this.state = { counter: 0 };
+  // }
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+  
   render() {
-    return <div>{this.props.color}</div>
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+      </div>
+    );
   }
 }
 
