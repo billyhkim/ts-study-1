a2a4c75bc70d0890a131df17cfdbc74837865ff3
Billy Kim <billyhkim.dev@gmail.com>
2020-03-30 16:13:25 -0700

    17 - adding interfaces to actions

--- 17-react-redux/actions/index.ts
+++ 17-react-redux/actions/index.ts
@@ -8,11 +8,16 @@ interface Todo {
   completed: boolean;
 }
 
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>('https://jsonplaceholder.typicode.com/todos');
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data,
     });
